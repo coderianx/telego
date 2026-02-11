b3e78dc1e3195b28211a79b9541cddf2e84669d0
Coderian <thecoderian.dev@gmail.com>
2026-02-11 20:05:57 +0300

    Add CallbackQuery and InlineKeyboard types
    Added CallbackQuery and InlineKeyboard types with relevant fields.

--- types.go
+++ types.go
@@ -1,8 +1,9 @@
 package telego
 
 type Update struct {
-	UpdateID int     `json:"update_id"`
-	Message  Message `json:"message"`
+	UpdateID      int            `json:"update_id"`
+	Message       *Message       `json:"message"`
+	CallbackQuery *CallbackQuery `json:"callback_query"`
 }
 
 type Message struct {
@@ -14,11 +15,29 @@ type Message struct {
 }
 
 type Chat struct {
-	ID int64 `json:"id"`
+	ID   int64  `json:"id"`
+	Type string `json:"type"`
 }
 
 type User struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
 	Username  string `json:"username"`
+}
+
+type CallbackQuery struct {
+	ID      string   `json:"id"`
+	From    User     `json:"from"`
+	Data    string   `json:"data"`
+	Message *Message `json:"message"`
+}
+
+type InlineKeyboardButton struct {
+	Text         string `json:"text"`
+	CallbackData string `json:"callback_data"`
+	URL          string `json:"url,omitempty"`
+}
+
+type InlineKeyboardMarkup struct {
+	InlineKeyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
 }
